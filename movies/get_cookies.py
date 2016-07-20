68e71b7f77bb123753df017bd539117b21a71b01
Manas <manas.daruka@gmail.com>
2016-07-21 00:46:48 +0530

    Added a user agent header

--- movies/get_cookies.py
+++ movies/get_cookies.py
@@ -5,7 +5,7 @@ def get_cookies(location):
 	website="https://in.bookmyshow.com/"+location+"/movies"
 	cj=cookielib.CookieJar()
 	opener=build_opener(HTTPCookieProcessor(cj),HTTPHandler)
-	req=Request(website)
+	req=Request(website, headers={'User-Agent' : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30"})
 	f=opener.open(req)
 	cookies={}
 	for cookie in cj:
